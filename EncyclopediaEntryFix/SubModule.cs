21af8e544bf2183d2e8b70236162358e132075c8
Designer225 <designer2553@gmail.com>
2021-06-17 23:13:50 -0700

    e1.6.0
    Looks like TaleWorlds finally fixed the original reason this mod was made.

--- EncyclopediaEntryFix/SubModule.cs
+++ EncyclopediaEntryFix/SubModule.cs
@@ -18,6 +18,7 @@ using TaleWorlds.MountAndBlade.GauntletUI;
 using TaleWorlds.MountAndBlade.View;
 using TaleWorlds.Engine;
 using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement;
+using TaleWorlds.Library;
 
 namespace EncyclopediaEntryFix
 {
@@ -31,37 +32,37 @@ namespace EncyclopediaEntryFix
         }
     }
 
-    [HarmonyPatch(typeof(DefaultEncyclopediaHeroPage), nameof(DefaultEncyclopediaHeroPage.GetListItems))]
-    public static class GetListItems_Patch
-    {
-        [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var codes = instructions.ToList();
-            int stage = 0;
-            for (int i = 0; i < codes.Count; i++)
-            {
-                if (stage == 0 && codes[i].opcode == OpCodes.Callvirt
-                    && codes[i].operand is MethodInfo && (codes[i].operand as MethodInfo) == AccessTools.PropertyGetter(typeof(Hero), nameof(Hero.Age)))
-                {
-                    codes[i].operand = AccessTools.PropertyGetter(typeof(Hero), nameof(Hero.IsChild));
-                    stage++;
-                }
-                else if (stage == 1 && codes[i].opcode == OpCodes.Ldc_R4)
-                {
-                    codes[i] = new CodeInstruction(OpCodes.Nop);
-                    stage++;
-                }
-                else if (stage == 2 && codes[i].opcode == OpCodes.Blt_Un_S)
-                {
-                    codes[i].opcode = OpCodes.Brtrue;
-                    break;
-                }
-            }
+    //[HarmonyPatch(typeof(DefaultEncyclopediaHeroPage), nameof(DefaultEncyclopediaHeroPage.GetListItems))]
+    //public static class GetListItems_Patch
+    //{
+    //    [HarmonyTranspiler]
+    //    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    //    {
+    //        var codes = instructions.ToList();
+    //        int stage = 0;
+    //        for (int i = 0; i < codes.Count; i++)
+    //        {
+    //            if (stage == 0 && codes[i].opcode == OpCodes.Callvirt
+    //                && codes[i].operand is MethodInfo && (codes[i].operand as MethodInfo) == AccessTools.PropertyGetter(typeof(Hero), nameof(Hero.Age)))
+    //            {
+    //                codes[i].operand = AccessTools.PropertyGetter(typeof(Hero), nameof(Hero.IsChild));
+    //                stage++;
+    //            }
+    //            else if (stage == 1 && codes[i].opcode == OpCodes.Ldc_R4)
+    //            {
+    //                codes[i] = new CodeInstruction(OpCodes.Nop);
+    //                stage++;
+    //            }
+    //            else if (stage == 2 && codes[i].opcode == OpCodes.Blt_Un_S)
+    //            {
+    //                codes[i].opcode = OpCodes.Brtrue;
+    //                break;
+    //            }
+    //        }
 
-            return codes.AsEnumerable();
-        }
-    }
+    //        return codes.AsEnumerable();
+    //    }
+    //}
 
     [HarmonyPatch(typeof(HeroViewModel), nameof(HeroViewModel.FillFrom))]
     public static class HeroViewModel_FillFromPatch
@@ -118,21 +119,37 @@ namespace EncyclopediaEntryFix
     public static class CreateImageWithIdPatch
     {
         // The original method is async, but the problem spot is not, so...
-        public static void Postfix(ImageIdentifierTextureProvider __instance, string id, int typeAsInt, string additionalArgs, ref CharacterCode ____characterCode)
+        public static void Postfix(ImageIdentifierTextureProvider __instance, string id, int typeAsInt, string additionalArgs, ref CharacterCode ____characterCode, ref bool ____creatingTexture)
         {
             if (typeAsInt == 5)
             {
                 CharacterCode from = CharacterCode.CreateFrom(id);
                 ____characterCode = from;
-
                 if (TaleWorlds.Core.FaceGen.GetMaturityTypeWithAge(from.BodyProperties.Age) <= BodyMeshMaturityType.Child && from.BodyProperties.Age >= 3)
+                {
+                    ____creatingTexture = true;
                     TableauCacheManager.Current.BeginCreateCharacterTexture(from, new Action<Texture>(__instance.OnTextureCreated), __instance.IsBig);
+                }
             }
         }
 
         private static void OnTextureCreated(this ImageIdentifierTextureProvider instance, Texture texture)
         {
             AccessTools.Method(typeof(ImageIdentifierTextureProvider), "OnTextureCreated").Invoke(instance, new object[] { texture });
+        }
+    }
+
+    [HarmonyPatch(typeof(ImageIdentifierTextureProvider), nameof(ImageIdentifierTextureProvider.ReleaseCache))]
+    public static class ReleaseCachePatch
+    {
+        public static void Postfix(ImageIdentifierTextureProvider __instance, CharacterCode ____characterCode)
+        {
+            if (__instance.ImageTypeCode == 5
+                && TaleWorlds.Core.FaceGen.GetMaturityTypeWithAge(____characterCode.BodyProperties.Age) <= BodyMeshMaturityType.Child
+                && ____characterCode.BodyProperties.Age >= 3)
+            {
+                TableauCacheManager.Current.ReleaseTextureWithId(____characterCode, __instance.IsBig);
+            }
         }
     }
 
